93426fb0777f75cb37d41895f9f3b6e061e175ef
silellak <silellak@gmail.com>
2026-09-02 10:29:07 -0700

    Lesson completed through 1.9

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express, { type Express, type Request, type Response } from 'express';
+import express from "express";
 
-const app: Express = express();
-const port = 8080;
+const app = express();
+const PORT = 8080;
 
-app.listen(port, () => {
-  console.log(`Chirpy listening on port ${port}`);
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
